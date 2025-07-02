400210c3543a092afdf7de5aa51ad8f7e3e5dc99
codeboy <mail@the-codeboy.com>
2025-07-02 13:45:56 +0200

    add basic logging and error handling

--- main.py
+++ main.py
@@ -9,11 +9,15 @@ def main():
     feeds = config["feeds"]
 
     for feed in feeds:
-        f = feedparser.parse(feed["url"])
-        for entry in f["entries"]:
-            add_entry(
-                config, entry["link"], token, feed["tags"] if "tags" in feed else None
-            )
+        try:
+            f = feedparser.parse(feed["url"])
+            for entry in f["entries"]:
+                add_entry(
+                    config, entry["link"], token, feed["tags"] if "tags" in feed else None
+                )
+            print(f"added {len(f['entries'])} articles from feed {feed['url']}")
+        except Exception:
+            print(f"failed to add entries for feed {feed['url']}")
 
 
 if __name__ == "__main__":
